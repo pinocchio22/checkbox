b9dc98216af9c65b565fff10104f7b445613f978
pinocchio22 <vviian.2@gmail.com>
2021-10-14 14:57:17 +0900

    first
    added RecyclerAdapterPhoto

---
+++ app/src/main/java/com/example/checkbox/RecyclerAdapterPhoto.kt
@@ -0,0 +1,62 @@
+package com.example.checkbox
+
+import android.app.Activity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.CheckBox
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+
+/**
+ * @author CHOI
+ * @email vviian.2@gmail.com
+ * @created 2021-10-14
+ * @desc
+ */
+class RecyclerAdapterPhoto(val context: Activity?, var list: ArrayList<thumbnailData>) : RecyclerView.Adapter<RecyclerAdapterPhoto.Holder>
+{
+    private lateinit var view : View
+
+    inner class Holder(itemView : View?) : RecyclerView.ViewHolder(itemView!!) {
+        var thumbnail : ImageView = itemView!!.findViewById(R.id.thumbnail)
+        var checkBox : CheckBox = itemView!!.findViewById(R.id.checkbox)
+
+        fun bind(data : thumbnailData, num : Int) {
+            val layoutParam = thumbnail.layoutParams as ViewGroup.MarginLayoutParams
+            thumbnail.layoutParams.width = size
+            thumbnail.layoutParams.height = size
+            layoutParam.setMargins(padding_size, padding_size, padding_size, padding_size)
+
+            thumbnail.setImageResource(0)
+            // 이미지를 생성하여 삽입하는 작업 필요함
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        view = LayoutInflater.from(context).inflate(R.layout.thumbnail_imgview, parent, false)
+        return Holder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        holder.bind(list[position], position)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
