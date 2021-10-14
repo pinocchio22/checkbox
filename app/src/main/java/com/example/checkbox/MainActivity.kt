9735014ccf664888e931c58e0730ffd83efda813
pinocchio22 <vviian.2@gmail.com>
2021-10-14 15:16:08 +0900

    feat : recyclerview
    added layout

--- app/src/main/java/com/example/checkbox/MainActivity.kt
+++ app/src/main/java/com/example/checkbox/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.checkbox
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var recyclerAdapter: RecyclerAdapterPhoto
+    private lateinit var recyclerView : RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recyclerView = findViewById(R.id.photo_recyclerView)
+        recyclerAdapter = RecyclerAdapterPhoto(this, lst)
+        recyclerView.adapter = recyclerAdapter
+        list = recyclerAdapter.getThumbnailList()
+        val lm = GridLayoutManager(Main_PhotoView(), photo_type)
+        recyclerView.layoutManager = lm
     }
 }
