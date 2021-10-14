79bde2f3ac259439b77b95753604d69564ecfe1f
pinocchio22 <vviian.2@gmail.com>
2021-10-14 16:32:19 +0900

    feat : MainPhotoView.kt
    added fun deletePhotoDlg in MainPhotoView.kt

--- app/src/main/java/com/example/checkbox/MainPhotoView.kt
+++ app/src/main/java/com/example/checkbox/MainPhotoView.kt
@@ -1,11 +1,13 @@
 package com.example.checkbox
 
+import android.content.DialogInterface
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import kotlinx.android.synthetic.main.recyclerview.*
+import kotlinx.android.synthetic.main.main_photoview.*
 
 /**
  * @author CHOI
@@ -41,6 +43,46 @@ class MainPhotoView : AppCompatActivity() {
         photolist_deletecancel.setOnClickListener {
             btck(0)
         }
+        photolist_deleteok.setOnClickListener {
+            deletePhotoDlg()
+        }
+    }
+
+    fun deletePhotoDlg() {
+        val warningBuilder : androidx.appcompat.app.AlertDialog.Builder = androidx.appcompat.app.AlertDialog.Builder(
+            this, // 경고 다이얼로그
+        android.R.style.Theme_DeviceDefault_Light_Dialog_NoActionBar_MinWidth
+        )
+        warningBuilder.setTitle("알림")   //제목
+        warningBuilder.setMessage("체크된 사진들을 모두 삭제합니다. \n정말 삭제하시겠습니까?")  //메세지
+        warningBuilder.setCancelable(false)
+        warningBuilder.setPositiveButton(
+            "확인", DialogInterface.OnClickListener { dialog, which ->
+                var i = 0
+                var j = 0
+                while (checkboxList.size != 0 && i < checkboxList.size) {
+                    if (checkboxList[i].checked) {
+                        val temp = checkboxList[i].id
+                        list.removeAt(i)
+                        checkboxList.removeAt(i)
+                        recyclerAdapter.notifyDataSetChanged()
+                        j = 1
+                    }
+                    else
+                        i++
+                }
+                if (j == 1)
+                    Toast.makeText(this, "사진이 삭제 완료 되었습니다.", Toast.LENGTH_SHORT)
+                else
+                    Toast.makeText(this, "삭제할 사진을 체크해주세요", Toast.LENGTH_SHORT)
+            }
+        )
+        warningBuilder.setNegativeButton(
+            "취소", DialogInterface.OnClickListener{ dialog, which ->
+                dialog.cancel()
+            })
+        val dlgWarning = warningBuilder.create()
+        dlgWarning.show()
     }
 
     private fun btck(n : Int) {
