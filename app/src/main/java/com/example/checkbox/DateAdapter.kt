1c6be06b6e10b14af11f768526567fa82ae7ecb5
pinocchio22 <vviian.2@gmail.com>
2021-10-22 16:13:34 +0900

    feat : layout
    added calendar_day_layout.xml

--- app/src/main/java/com/example/checkbox/DateAdapter.kt
+++ app/src/main/java/com/example/checkbox/DateAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.checkbox
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
@@ -16,8 +17,17 @@ import kotlin.collections.ArrayList
 class DateAdapter(context : FragmentActivity, size : Pair<Int, Int>?, days : ArrayList<Date>, inputMonth : Int, val itemClick : (Date, Int) -> Unit) :
         ArrayAdapter<Date>(context, R.layout.fragment_cal, days) {
 
+    private val inflater : LayoutInflater = LayoutInflater.from(context)
+
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        
-        return super.getView(position, convertView, parent)
+        var view = convertView
+        val calendar = Calendar.getInstance()
+        val date = getItem(position)!!
+
+        calendar.time = date
+        val month = calendar.get(Calendar.MONTH)
+        val week = calendar.get(Calendar.DAY_OF_WEEK)
+
+        if (view == null) view = inflater.inflate(R.layout.cal)
     }
 }
