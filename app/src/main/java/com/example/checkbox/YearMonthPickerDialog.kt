75c425f447b88c54baa1c648eff2fb6e6f0b868a
pinocchio22 <vviian.2@gmail.com>
2021-10-25 15:59:50 +0900

    feat : YearMonthPickerDialog.kt
    overrode fun onCreateDialog

--- app/src/main/java/com/example/checkbox/YearMonthPickerDialog.kt
+++ app/src/main/java/com/example/checkbox/YearMonthPickerDialog.kt
@@ -6,8 +6,12 @@ import android.app.Dialog
 import android.os.Build
 import android.os.Bundle
 import android.view.View
+import android.widget.NumberPicker
 import androidx.annotation.RequiresApi
+import androidx.appcompat.widget.AppCompatImageButton
 import androidx.fragment.app.DialogFragment
+import kotlinx.android.synthetic.main.search_view.view.*
+import java.util.*
 
 /**
  * @author CHOI
@@ -15,8 +19,13 @@ import androidx.fragment.app.DialogFragment
  * @created 2021-10-25
  * @desc
  */
-class YearMonthPickerDialog<Button : View?>(v : View, tag : String) : DialogFragment {
+class YearMonthPickerDialog<Button : View?>(v : View, tag : String) : DialogFragment() {
     private var listener : DatePickerDialog.OnDateSetListener? = null
+    val tagname : String = tag
+    val v = v
+    var cal = Calendar.getInstance()
+    private var MAX_YEAR = 2099
+    private var MIN_YEAR = 1980
 
     fun setListener(listener : DatePickerDialog.OnDateSetListener?) {
         this.listener = listener
@@ -33,6 +42,35 @@ class YearMonthPickerDialog<Button : View?>(v : View, tag : String) : DialogFrag
             btnConfirm = it.findViewById<Button>(R.id.btn_confirm)
             btnCancel = it.findViewById<Button>(R.id.btn_cancel)
         }
+
+        val monthPicker = dialog.findViewById<View>(R.id.picker_month) as NumberPicker
+        val yearPicker = dialog.findViewById<View>(R.id.picker_year) as NumberPicker
+        btnConfirm?.setOnClickListener(View.OnClickListener {
+            if (tagname == "calendar") {
+                val y = DateFragment.calDate.get(Calendar.YEAR)
+                val m = DateFragment.calDate.get(Calendar.MONTH)
+                DateFragment.calDate.add(Calendar.MONTH, 12 * (yearPicker.value.toInt() - y) + (monthPicker.value.toInt() - m))
+                val month_left_button = v.findViewById<AppCompatImageButton>(R.id.cal_month_left)
+                month_left_button?.performClick()
+            } else if (tagname == "search") {
+                if (monthPicker.value < 10) v.searchview.setQuery("${yearPicker.value}" + "" + "0${monthPicker.value}", true)
+                else v.searchview.setQuery("${yearPicker.value}" + "" + "${monthPicker.value}", true)
+            }
+            dismiss()
+        })
+        btnCancel?.setOnClickListener(View.OnClickListener {
+            dismiss()
+        })
+        monthPicker.minValue = 1
+        monthPicker.maxValue = 12
+        monthPicker.value = cal[Calendar.MONTH] + 1
+        val year = cal[Calendar.YEAR]
+        yearPicker.minValue = MIN_YEAR
+        yearPicker.maxValue = MAX_YEAR
+        yearPicker.value = year
+        builder.setView(dialog)
+
+        return builder.create()
     }
 }
 
