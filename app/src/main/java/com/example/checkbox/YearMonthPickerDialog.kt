10babad330df294a85ead0a468b572c45ad6cfe7
pinocchio22 <vviian.2@gmail.com>
2021-10-25 15:40:43 +0900

    feat : YearMonthPickerDialog.kt
    added fun setListener

--- app/src/main/java/com/example/checkbox/YearMonthPickerDialog.kt
+++ app/src/main/java/com/example/checkbox/YearMonthPickerDialog.kt
@@ -1,10 +1,19 @@
 package com.example.checkbox
 
+import android.app.DatePickerDialog
+import android.view.View
+import androidx.fragment.app.DialogFragment
+
 /**
  * @author CHOI
  * @email vviian.2@gmail.com
  * @created 2021-10-25
  * @desc
  */
-class YearMonthPickerDialog {
+class YearMonthPickerDialog<Button : View?>(v : View, tag : String) : DialogFragment {
+    private var listener : DatePickerDialog.OnDateSetListener? = null
+
+    fun setListener(listener : DatePickerDialog.OnDateSetListener?) {
+        this.listener = listener
+    }
 }
