8b1ae7899830e2220cac1222ccd499041055e416
pinocchio22 <vviian.2@gmail.com>
2021-10-26 15:50:11 +0900

    feat : SearchView.kt
    overrode fun onCreate

--- app/src/main/java/com/example/checkbox/SearchView.kt
+++ app/src/main/java/com/example/checkbox/SearchView.kt
@@ -1,7 +1,12 @@
 package com.example.checkbox
 
+import android.os.Build
 import android.os.Bundle
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProviders
+import kotlinx.android.synthetic.main.main_photoview.*
+import kotlinx.android.synthetic.main.search_view.*
 
 /**
  * @author CHOI
@@ -10,8 +15,20 @@ import androidx.appcompat.app.AppCompatActivity
  * @desc
  */
 class SearchView : AppCompatActivity() {
+
+    private lateinit var vm : PhotoViewModel
+
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.search_view)
+        searchview.queryHint = "CHECKBOX 검색"
+        searchview.onActionViewExpanded()
+        searchview.isIconified = false
+        vm = ViewModelProviders.of(this).get(PhotoViewModel::class.java)
+        dateQuery()
+        searchResult()
     }
+
+
 }
