b61adafe07be1db6c31855b0de1a7d36c2e8822d
pinocchio22 <vviian.2@gmail.com>
2021-10-29 14:54:54 +0900

    feat : PagerRecyclerAdapter.kt
    overrode fun instantiateItem

--- app/src/main/java/com/example/checkbox/PagerRecyclerAdapter.kt
+++ app/src/main/java/com/example/checkbox/PagerRecyclerAdapter.kt
@@ -1,8 +1,13 @@
 package com.example.checkbox
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.viewpager.widget.PagerAdapter
+import androidx.viewpager.widget.ViewPager
+import android.view.LayoutInflater as LayoutInflater1
 
 /**
  * @author CHOI
@@ -11,6 +16,8 @@ import androidx.viewpager.widget.PagerAdapter
  * @desc
  */
 class PagerRecyclerAdapter (parivate val context : Context, var list : ArrayList<thumbnailData>, var tb : View, var bt : View) : PagerAdapter() {
+    private var layoutInflater : LayoutInflater1? = null
+    private var check : Boolean = false
 
     override fun isViewFromObject(view: View, `object`: Any): Boolean {
         return view === `object`    // '===' 는참조 타입의 주소 값을 비교
@@ -20,4 +27,39 @@ class PagerRecyclerAdapter (parivate val context : Context, var list : ArrayList
         return list.size
     }
 
+    override fun instantiateItem(container: ViewGroup, position: Int): Any {
+        layoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater1
+        val v = layoutInflater!!.inflate(R.layout.photoview_pager, null)
+        val image = v.findViewById<View>(R.id.imgView) as ImageView
+        val vp = container as ViewPager
+        vp.addView(v, 0)
+        ImageLoder.execute(ImageLode(context, image, list[position].photo_id, 0))
+        image.setOnClickListener(object : View.OnClickListener{
+            override fun onClick(v: View?) {
+                if (check == false) {
+                    tb.visibility = View.GONE
+                    bt.visibility = View.GONE
+                    check = true
+                } else {
+                    tb.visibility = View.VISIBLE
+                    bt.visibility = View.VISIBLE
+                    check = false
+                }
+            }
+        })
+        return v
+    }
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
